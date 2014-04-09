4e9f7c48655f111ae08af760e5e22d0803baedd5
peiliping <peilipingplp@gmail.com>
2014-04-09 17:34:18 +0800

    update

--- src/main/java/peiliping/Test.java
+++ src/main/java/peiliping/Test.java
@@ -1,13 +1,34 @@
 package peiliping;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import eu.bitwalker.useragentutils.Browser;
+
+
 
 
 public class Test {
-	
-	public static void main(String[] args) {
-		
-	    long a = 9223372036854775744L;
-	    System.out.println(Long.MAX_VALUE-a);
-		
-	}
+
+    public static void main(String[] args) {        
+        
+        ArrayList<Browser> l = new ArrayList<Browser>();
+        
+        
+        for (Browser b : Browser.values()){
+            l.add(b);
+        }
+        
+        Collections.sort(l, new Comparator<Browser>() {
+            @Override
+            public int compare(Browser o1, Browser o2) {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+        });
+        
+        
+        
+    }
 }
